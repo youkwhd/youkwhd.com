4b91a969ebf227f9b4e7cf126145e9be0eab71dd
youkwhd <youkwhd@gmail.com>
2021-09-07 13:16:00 +0700

    add title props

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "gatsby";
 
-const Layout = ({ children }) => {
+const Layout = ({ title, children }) => {
     const rootPath = `${__PATH_PREFIX__}/`;
 
     return (
         <div className="global-wrapper">
             <header className="global-header">
                 <h1 className="main-heading">
-                    <Link to="/">youkwhd</Link>
+                    <Link to="/">{title}</Link>
                 </h1>
             </header>
             <main>{children}</main>
