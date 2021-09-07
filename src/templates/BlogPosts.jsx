016c5605c41d5d587ad246215c0bfe68caec7b99
youkwhd <youkwhd@gmail.com>
2021-09-07 19:44:18 +0700

    add dynamic gatsby image

--- src/templates/BlogPosts.jsx
+++ src/templates/BlogPosts.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 
 import Layout from "../components/Layout/Layout";
 
 const BlogPosts = ({ data, location }) => {
     const post = data.mdx;
+    const featuredImage = getImage(post.frontmatter.featuredImage);
+    const imageExist = featuredImage !== undefined;
 
     return (
         <Layout location={location} title={data.site.siteMetadata.title}>
             <span className="post-category">{post.frontmatter.category.toUpperCase()}</span>
             <h1>{post.frontmatter.title}</h1>
+            { imageExist && <GatsbyImage image={featuredImage} alt="featured image"/> }
             <ul className="post-author">
                 <li>written by {post.frontmatter.author}</li>
                 <li>{post.frontmatter.date}</li>
@@ -36,6 +40,11 @@ export const pageQuery = graphql`
             frontmatter {
                 author
                 category
+                featuredImage {
+                    childImageSharp {
+                        gatsbyImageData(layout: FULL_WIDTH)
+                    }
+                }
                 date(formatString: "D MMM, YYYY")
                 title
             }
