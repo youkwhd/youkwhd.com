f0752a1c603080987ae77c9ccc8994fc0d9efa70
youkwhd <youkwhd@gmail.com>
2021-09-09 10:07:43 +0700

    add new page

---
+++ src/pages/contact.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { graphql } from "gatsby";
+
+import Layout from "../components/Layout";
+import Seo from "../components/Seo";
+
+const Contact = ({ data, location }) => {
+    return (
+        <Layout location={location} title={data.site.siteMetadata.title}>
+            <Seo title="Contact" />
+            hello from Contact
+        </Layout>
+    );
+};
+
+export default Contact;
+
+export const pageQuery = graphql`
+	query {
+		site {
+			siteMetadata {
+				title
+			}
+		}
+	}
+`;
