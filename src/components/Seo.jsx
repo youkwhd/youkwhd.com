877ae707026ffcf715195a32af5fd198bb8d3da8
youkwhd <youkwhd@gmail.com>
2021-09-12 17:28:41 +0700

    lowercase title

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -29,7 +29,7 @@ const Seo = ({ description, lang, meta, title }) => {
                 lang
             }}
             title={title}
-            titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+            titleTemplate={defaultTitle ? `%s | ${defaultTitle}`.toLowerCase() : null}
             meta={[
                 {
                     name: `description`,
