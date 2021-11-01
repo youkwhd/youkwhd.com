98780ea0910a1b4f61dfdad9175baa909a187eb0
youkwhd <youkwhd@gmail.com>
2021-11-01 22:26:09 +0700

    add padding for 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,11 +5,11 @@ import Seo from "../components/Seo";
 
 const NotFoundPage = () => {
     return (
-		<>
+		<div style={{ padding: "var(--spacing-0) var(--spacing-3)" }}>
 			<Seo title="404: not found" />
 			<h1>sorry there is no road ahead :(</h1>
 			<p>it seems that you are lost, <Link to="/">go back</Link></p>
-		</>
+		</div>
 	);
 };
 
