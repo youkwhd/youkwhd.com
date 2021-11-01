ace7b9a4921e96d50437646b7d2ab1145a992a6a
youkwhd <youkwhd@gmail.com>
2021-11-01 22:21:39 +0700

    re-config 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
 
-import Layout from "../components/Layout";
-import Seo from "../components/Seo"
+import Seo from "../components/Seo";
 
-const NotFoundPage = ({ data, location }) => {
+const NotFoundPage = () => {
     return (
-		<Layout location={location} title={data.site.siteMetadata.title}>
+		<>
 			<Seo title="404: not found" />
 			<h1>sorry there is no road ahead :(</h1>
 			<p>it seems that you are lost, <Link to="/">go back</Link></p>
-		</Layout>
+		</>
 	);
 };
 
