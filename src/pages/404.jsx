58c653068ddd0ac364302bf6843819d2f3a408ad
youkwhd <youkwhd@gmail.com>
2021-11-01 23:03:08 +0700

    globally add padding

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,8 +5,8 @@ import Seo from "../components/Seo";
 
 const NotFoundPage = () => {
     return (
-		<div style={{ padding: "var(--spacing-0) var(--spacing-3)" }}>
-			<Seo title="404: not found" />
+		<div className="text-wrapper">
+            <Seo title="404: not found" />
 			<h1>sorry there is no road ahead :(</h1>
 			<p>it seems that you are lost, <Link to="/">go back</Link></p>
 		</div>
