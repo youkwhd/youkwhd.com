f6a3d0c2e439c34f7f02089e00930408a39af4fa
youkwhd <youkwhd@gmail.com>
2021-11-01 22:12:32 +0700

    add new font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
 import "./src/assets/css/global.css";
 import "./src/assets/css/underline.css";
+import "./src/assets/fonts/anonymous/anonymous.css";
 
 require("./src/assets/css/tomorrow-night.css");
