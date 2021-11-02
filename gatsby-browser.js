93020546cd55225652cc89794aafe865db33c495
youkwhd <youkwhd@gmail.com>
2021-11-02 22:05:46 +0700

    remove underline.css import

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,4 @@
 import "./src/assets/css/global.css";
-import "./src/assets/css/underline.css";
 import "./src/assets/fonts/anonymous/anonymous.css";
 
 require("./src/assets/css/tomorrow-night.css");
