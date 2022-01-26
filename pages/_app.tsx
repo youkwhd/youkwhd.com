aedd42a55eb44919817c087c26e93326a77e595e
youkwhd <youkwhd@gmail.com>
2022-01-26 21:12:46 +0700

    add nextjs initial project

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+const _app = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+
+export default _app; 
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+	<p>hello world</p>
+  );
+}
+
+export default Home;
