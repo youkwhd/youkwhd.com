c1007dce5d03a34e549840bc29f29986e68a75fc
youkwhd <youkwhd@gmail.com>
2022-01-27 15:52:11 +0700

    fix tab spaces

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -43,7 +43,7 @@ export async function getStaticPaths() {
         paths: posts.map((post) => {
             return {
                 params: {
-                slug: post.slug,
+                    slug: post.slug,
                 },
             }
         }),
