aeca89804e31eb5d6dfb308e84a0a529a767ca4a
youkwhd <lolywk@tutanota.com>
2022-03-12 10:32:35 +0700

    add prism as code highlighter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import '../styles/prism.css';
 import type { AppProps } from 'next/app';
 
 const _app = ({ Component, pageProps }: AppProps): JSX.Element => <Component {...pageProps} />;
--- utils/markdownConverter.ts
+++ utils/markdownConverter.ts
@@ -3,20 +3,20 @@
  * e.g. markdown content; "hello, **world**"
  *
  */
-
 import { remark } from "remark";
 import remarkHtml from "remark-html";
 import remarkToc from "remark-toc";
 
 import { rehype } from "rehype";
 import rehypeSlug from "rehype-slug";
+import rehypePrism from "@mapbox/rehype-prism";
 
 export async function addIDToHTML(html: string) {
-    return (await rehype().data("settings", { fragment: true }).use(rehypeSlug).process(html)).toString();
+    return (await rehype().data("settings", { fragment: true }).use(rehypeSlug).use(rehypePrism).process(html)).toString();
 }
 
 export async function markdownToHTML(markdown: string) {
-    const convertedHTML = (await remark().use(remarkToc).use(remarkHtml).process(markdown)).toString();
+    const convertedHTML = (await remark().use(remarkToc).use(remarkHtml, { sanitize: false }).process(markdown)).toString();
     return addIDToHTML(convertedHTML);
 }
 
