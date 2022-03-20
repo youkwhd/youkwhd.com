b761bb1c06e028122e93d00714028e80e23a6176
youkwhd <lolywk@tutanota.com>
2022-03-20 15:13:45 +0700

    add JetBrains Mono as primary font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
+import "../styles/font-types/jetbrains-mono.css";
 import "../styles/main.css";
-// import "../styles/prism-themes/prism-atom-dark.css";
-import "../styles/prism-themes/prism-gruvbox-dark.css"
+import "../styles/prism-themes/prism-gruvbox-dark.css";
 import type { AppProps } from "next/app";
 
 const _app = ({ Component, pageProps }: AppProps): JSX.Element => <Component {...pageProps} />;
