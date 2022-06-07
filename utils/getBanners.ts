513970aec3408b5fb84081e543ffb19e4da4c9e3
youkwhd <lolywk@tutanota.com>
2022-06-07 22:32:36 +0700

    style: attempt to fix spaces

--- utils/getBanners.ts
+++ utils/getBanners.ts
@@ -4,15 +4,15 @@ import { Banner } from "../types";
 import replaceString from "./replaceString";
 
 const getAllBanners = (): Banner[] => {
-	const bannersDir: string = join(process.cwd(), "public/images/banners");
-	const banners: string[] = fs.readdirSync(bannersDir);
+    const bannersDir: string = join(process.cwd(), "public/images/banners");
+    const banners: string[] = fs.readdirSync(bannersDir);
     const parsedBanners: Banner[] = banners.map((b: string): Banner => parseBanner(b));
 
     return parsedBanners.sort((banner1, banner2) => banner1.index - banner2.index);
 };
 
 const parseBanner = (banner: string): Banner => {
-	const tempBanner: string[] = banner.split("[-]");
+    const tempBanner: string[] = banner.split("[-]");
     let tempUrl: string = tempBanner[1];
 
     tempUrl = replaceString(tempUrl, "[slash]", "/");
