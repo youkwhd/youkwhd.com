228a9541495d687a4e185eac04d9329c8a70511b
youkwhd <lolywk@tutanota.com>
2022-08-08 18:42:54 +0700

    chore: new look for the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,8 @@ import Link from "next/link";
 import { PageConfig } from "next";
 import { NextSeo } from "next-seo";
 
-import { Post, Banner } from "../types";
+import { Banner } from "../types";
 import { MainLayout } from "../components/Layout";
-import { getAllPosts } from "../utils/getPosts";
 import { getAllBanners } from "../utils/getBanners";
 import { generateRSSFeed } from "../scripts/generateRSSFeed";
 
@@ -13,11 +12,10 @@ export const config: PageConfig = {
 };
 
 type Props = {
-    recentPosts: Post[];
     banners: Banner[];
 };
 
-const Home = ({ recentPosts, banners }: Props): JSX.Element => {
+const Home = ({ banners }: Props): JSX.Element => {
     return (
         <>
             <NextSeo
@@ -25,34 +23,27 @@ const Home = ({ recentPosts, banners }: Props): JSX.Element => {
                 description="a personal blog maintained by youkwhd, contents are mostly about GNU/Linux or programming."
             />
             <MainLayout banners={banners}>
-                <h1>whoami</h1>
+                <h1 className="_fake_h2">Introducing youkwhd</h1>
                 <p>
-                    I'm an undergraduate student majoring Computer Science, but also known as Informatics in my country, Indonesia.
-                    I love programming at every aspect of it, it has been my hobby since i know the programming way to solve problems, plus the math involed in it. 
-                    I mean, it looks cool to write such program with Vim and seeing people thinking that you're some kind of a hackerman. I'm currently working with web apps,
-                    usually with React, and i primarily use Typescript as my go-to language alongside with Next.js, i'm also comfortable developing the backend using Node.js.
+                    I'm an undergraduate student majoring Computer Science, a fast paced self-taught programmer who's proficient using C99 & Typescript as a language of choice, utilizing Neovim as an
+                    efficient text editor for every day use.
                 </p>
                 <p>
-                    As a GNU/Linux enthusiast, I use Arch as my daily drive operating system. I wanted to use Gentoo cuz i'm a geek myself, plus Arch is 
-                    a rolling release distribution, i'm lazy. But it would be a pain in the ass to compile things from source. I stand with the movement of free and open source
-                    softwares (FOSS). As most of, if not, all of my projects are free and open source. You have to note that the term "free" and "open source" is different.
+                    With the mention of Neovim, thus can be summarized that i am in fact a GNU/Linux enthusiast. I prefer to use a non PID1 systemd based systems. Most of my softwares are purely Libre.
                 </p>
-                <h2>take a look at my blog</h2>
+                <h2>Writing articles</h2>
                 <p>
-                    Writing articles has also been one of my hobby. Some articles that i've wrote isn't guaranteed to be perfect in terms of writing. You can
-                    find <Link href={"/posts"}><a>all of my articles</a></Link> by clicking the link, or find it by checking <Link href={"/posts/tags"}><a>all the available tags</a></Link>, you can always 
-                    help me by contributing to this site and fix some problems i have on this site. Speaking of blog, here are some of my recent blog posts:
+                    The only reason why this site is here is because of me wanting to write articles. Be aware that each and every articles are written based on my experience. You can find my 
+                    articles by either:
                 </p>
-                <ul>
-                    {recentPosts.map((post: Post) => (
-                        <li key={post.slug}>
-                            <Link as={`/posts/${post.slug}`} href={"/posts/[slug]"}>
-                                {post.title}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>   
-                <h2>contact me</h2>
+                <ol>
+                    <li><Link href={"/posts"}><a>Inspecting every articles</a></Link></li>
+                    <li><Link href={"/posts/tags"}><a>Checking all the available tags/topics</a></Link></li>
+                </ol>
+                <p>
+                    Thank you for the visit, this site is free from Javascript.
+                </p>
+                <h2>Chat with me</h2>
                 <p>
                     Please contact me via email, the address is <Link href={"mailto:lolywk@tutanota.com"}><a>lolywk@tutanota.com</a></Link>,
                     grab my <Link href={"/pgp-public-key"}><a>pgp public key</a></Link> for convenience.
@@ -63,16 +54,12 @@ const Home = ({ recentPosts, banners }: Props): JSX.Element => {
 };
 
 export const getStaticProps = async () => {
-    const posts: Post[] = getAllPosts();
-    const recentPosts: Post[] = posts.slice(0, 3);
-    const banners: Banner[] = getAllBanners();
-
+    /* TODO: add .env variable for DEVELOPMENT */
     // await generateRSSFeed(posts);
 
     return {
         props: {
-            recentPosts,
-            banners
+            banners: getAllBanners()
         },
     };
 };
