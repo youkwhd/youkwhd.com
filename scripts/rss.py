303c1867fd2cac4e0a8febca2a297fa7638f5f61
youkwhd <lolywk@tutanota.com>
2022-10-28 10:44:56 +0700

    feat: WIP check if markdown has uploaded

--- scripts/rss.py
+++ scripts/rss.py
@@ -1,6 +1,7 @@
 import os
 import re
 import rfeed as rf
+import requests as req
 import datetime as date
 import markdown as md
 
@@ -10,34 +11,40 @@ posts_rss = []
 
 url = "https://youkwhd.vercel.app"
 
+# TODO: use git library or similiar
+pushed_files = req.get("https://github.com/youkwhd/youkwhd.vercel.app/tree/master/src/_posts")
+
 for post_file in posts_files:
-        with open(f'{posts_dir}/{post_file}') as f:
-            content = f.read()
-            r = re.sub(' -\\s', "   ", content)
-            r = re.sub(': "', ": ", r)
-            r = re.sub('"', "", r)
-            content = r
+    if post_file not in pushed_files.text:
+        continue
+
+    with open(f'{posts_dir}/{post_file}') as f:
+        content = f.read()
+        r = re.sub(' -\\s', "   ", content)
+        r = re.sub(': "', ": ", r)
+        r = re.sub('"', "", r)
+        content = r
 
 
-            md_conv = md.Markdown(extensions=["meta"])
-            html = md_conv.convert(content)
+        md_conv = md.Markdown(extensions=["meta"])
+        html = md_conv.convert(content)
 
-            metadata = md_conv.Meta
+        metadata = md_conv.Meta
 
-            post_slug = os.path.splitext(post_file)[0]
-            post_url = f'{url}/posts/{post_slug}'
-            post_date_split = metadata["date"][0].split("-")
-            post_date = date.datetime(int(post_date_split[0]), int(post_date_split[1]), int(post_date_split[2].split("T")[0]))
+        post_slug = os.path.splitext(post_file)[0]
+        post_url = f'{url}/posts/{post_slug}'
+        post_date_split = metadata["date"][0].split("-")
+        post_date = date.datetime(int(post_date_split[0]), int(post_date_split[1]), int(post_date_split[2].split("T")[0]))
 
-            rss_item = rf.Item(
-                    title=metadata["title"][0],
-                    link=post_url,
-                    description=metadata["excerpt"][0],
-                    guid=rf.Guid(post_url),
-                    pubDate=post_date
-                    )
+        rss_item = rf.Item(
+                title=metadata["title"][0],
+                link=post_url,
+                description=metadata["excerpt"][0],
+                guid=rf.Guid(post_url),
+                pubDate=post_date
+                )
 
-            posts_rss.append(rss_item)
+        posts_rss.append(rss_item)
 
 feed = rf.Feed(
         title="youkwhd's blog posts RSS",
