526e0bae9625ad9a3a549b37603258e0149a7478
youkwhd <lolywk@tutanota.com>
2022-12-12 11:29:29 +0700

    fix: import not found

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import Banner from "@/src/components/Banner"
-import type { Banner as BannerType } from "@/src/types"
+import type { Banner as BannerType } from "@/types"
 
 type Props = { banners: BannerType[] }
 export default ({ banners }: Props): JSX.Element => {
