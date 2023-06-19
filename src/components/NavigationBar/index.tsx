46c9d914f89b1900f3bd2658bb9a9b560dfb0d64
youkwhd <lolywk@tutanota.com>
2023-06-19 15:19:08 +0700

    chore: no span inside ul

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -3,17 +3,17 @@ import Link from "next/link"
 export default (): JSX.Element => {
     return (
         <ul className="navigation-bar">
-            <span className="navigation-bar--opener">{'['}</span>
-                <li>
-                    <Link href="/">Home</Link> |
-                </li>
-                <li>
-                    <Link href="/posts">Web Articles</Link> |
-                </li>
-                <li>
-                    <Link href="/pgp-public-key">PGP Key</Link>
-                </li>
-            <span className="navigation-bar--closer">{']'}</span>
+            <li>
+                <span className="navigation-bar--opener">{'['}</span>
+                <Link href="/">Home</Link> |
+            </li>
+            <li>
+                <Link href="/posts">Web Articles</Link> |
+            </li>
+            <li>
+                <Link href="/pgp-public-key">PGP Key</Link>
+                <span className="navigation-bar--closer">{']'}</span>
+            </li>
         </ul>
     )
 }
