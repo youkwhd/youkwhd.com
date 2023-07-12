604f6b40a6161dd16b8bf5b471d3e33da3b47bd3
youkwhd <lolywk@tutanota.com>
2023-07-12 14:44:20 +0700

    chore: log filename

--- scripts/check_config.py
+++ scripts/check_config.py
@@ -16,9 +16,9 @@ def main():
             continue
 
         if __PAGE_CONFIG__ in open(file, mode="r", encoding="utf-8").read():
-            log_success("OK")
+            log_success("OK", os.path.basename(file))
         else:
-            log_failure("Missing PageConfig")
+            log_failure("Missing PageConfig", os.path.basename(file))
 
 if __name__ == "__main__":
     main()
