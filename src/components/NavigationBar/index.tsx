252f32edae76303e7d16e3388dbe83c5b4027b55
youkwhd <lolywk@tutanota.com>
2023-07-15 22:37:50 +0700

    chore: simpler navbar

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -2,27 +2,24 @@ import Link from "next/link"
 
 export default (): JSX.Element => {
     return (
-        <ul className="navigation-bar">
-            <li>
-                <span className="navigation-bar--opener">{'['}</span>
-                <Link href="/">Home</Link> |
-            </li>
-            <li>
-                <Link href="/articles">Articles</Link> |
-            </li>
-            <li>
-                <Link href="/art">Art</Link> |
-            </li>
-            <li>
-                <Link href="/photos">Photos</Link> |
-            </li>
-            <li>
-                <Link href="/collections">Collections</Link> |
-            </li>
-            <li>
-                <Link href="/pgp-public-key">PGP Key</Link>
-                <span className="navigation-bar--closer">{']'}</span>
-            </li>
-        </ul>
+        <nav>
+            <ul className="navigation-bar">
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/articles">Articles</Link>
+                </li>
+                <li>
+                    <Link href="/art">Art</Link>
+                </li>
+                <li>
+                    <Link href="/photos">Photos</Link>
+                </li>
+                <li>
+                    <Link href="/collections">Collections</Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
