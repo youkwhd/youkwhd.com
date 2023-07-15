d4eb5c48b5ab3f0f8d523fe1a12887ad3a28c9a2
youkwhd <lolywk@tutanota.com>
2023-07-15 22:47:30 +0700

    chore: capitalize

--- src/pages/art/index.tsx
+++ src/pages/art/index.tsx
@@ -12,7 +12,7 @@ export default (): JSX.Element => {
 
             <p>I like to draw, was. I remembered that i used to draw things after my brain randomly gave me flashbacks of an old image of my drawing:</p>
             <img src="images/art_gundam.jpeg" loading="lazy" alt="" width={300} height={533} />
-            <p>This gundam art is essentially a copy from an image of gundam in real life, this art was made with an iphone.</p>
+            <p>This gundam art is essentially a copy from an image of gundam in real life, this art was made with an iPhone.</p>
             <p>With the second version having colors:</p>
             <img src="images/art_gundam_colored.png" loading="lazy" alt="" width={300} height={533} />
             <p>That "ywk" is a signature that this piece was made by me, youkwhd. I honestly like the gundam without proper hands, for some reason. It might be my weird taste on art.</p>
