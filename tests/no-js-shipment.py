583ad4c01ad02afe3e83b5d5524600d2935c2354
youkwhd <lolywk@tutanota.com>
2023-07-17 09:51:13 +0700

    refactor: why not read locally

--- tests/no-js-shipment.py
+++ tests/no-js-shipment.py
@@ -13,23 +13,26 @@ posixpath = str
 def posix_path(path: str) -> str:
     return path.replace('\\', '/')
 
-def generate_web_paths(url: str, entry_point: LiteralString, ignore: list[posixpath]) -> list[str]:
+def get_generated_html_files(entry_point: LiteralString, ignore: list[posixpath]) -> list[str]:
     paths = []
 
-    for root, _, _ in os.walk(entry_point):
-        if root == entry_point:
-            continue
-
+    for root, _, files in os.walk(entry_point):
         target_path = posix_path(os.path.relpath(root, entry_point))
         ignored = False
         
         for ignored_path in ignore:
             if target_path.startswith(ignored_path):
                 ignored = True
+                break
+
+        if ignored:
+            continue
+
+        for file in files:
+            if not file.endswith(".html"):
                 continue
 
-        if not ignored:
-            paths.append(url + target_path)
+            paths.append(os.path.join(root, file))
 
     return paths
 
@@ -37,7 +40,13 @@ if __name__ == "__main__":
     if not os.path.exists(os.path.join("dist")):
         subprocess.call("npx astro build", shell=True)
 
-    web_port = 8000
-    web_url = f"http://localhost:{web_port}/"
-    web_paths = generate_web_paths(web_url, os.path.join("dist"), ignore=["images", "_astro"])
+    files = get_generated_html_files(os.path.join("dist"), ignore=["images", "_astro"])
+
+    for file in files:
+        with open(file, mode="r", encoding="utf-8") as f:
+            contents = f.read()
+
+            assert ".js" not in contents
+            print("[no-js-shipment]: file passed")
+
 
