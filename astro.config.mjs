0fff89aaac597bb938087689c15ea05b4742002f
youkwhd <lolywk@tutanota.com>
2023-07-17 22:48:11 +0700

    feat: use prism syntax highlighter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config"
+import { rehypeHeadingIds } from '@astrojs/markdown-remark'
 
 export default defineConfig({
 	site: "https://youkwhd.com",
 	integrations: [],
-});
+    markdown: {
+        syntaxHighlight: "prism",
+        rehypePlugins: [
+            rehypeHeadingIds,
+        ],
+    },
+})
