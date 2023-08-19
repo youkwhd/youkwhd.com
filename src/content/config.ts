518afea05f6e3c0195caf152638b19a0baf0cbf3
youkwhd <lolywk@tutanota.com>
2023-08-20 00:40:15 +0700

    docs: no one cares

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,8 @@
 import { defineCollection, z } from "astro:content"
 
-/* should be named article as this is a single object,
- * this case, it cannot be so, since Astro syncs the object name
- * relative to the directory name.
+/* should be named post as this is a single object,
+ * this case, it cannot be so, since Astro syncs the object 
+ * name relative to the directory name.
  */
 const posts = defineCollection({
 	schema: z.object({
