203ef51ca018d928bad4f6d3728253319f2e087f
youkwhd <lolywk@tutanota.com>
2023-09-09 19:24:02 +0700

    chore: add indonesian song

--- data/collections/songs/indonesia/index.ts
+++ data/collections/songs/indonesia/index.ts
@@ -8,6 +8,10 @@ export default [
         url: "https://open.spotify.com/track/6XsFgTG4dY768oIB4Dmeu0",
     },
     {
+        name: "Feby Putri - Halu",
+        url: "https://open.spotify.com/track/3iszMeG87RxYujbKieNNRQ",
+    },
+    {
         name: "Peterpan - Cobalah Mengerti",
         url: "https://www.youtube.com/watch?v=atGEIFsuLpA",
     },
