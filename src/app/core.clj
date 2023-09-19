9f5608840d0d9e828cfb67fe0f527e3c197bd43e
youkwhd <lolywk@tutanota.com>
2023-09-19 23:03:24 +0700

    feat: generate 404 page

--- src/app/core.clj
+++ src/app/core.clj
@@ -1,15 +1,24 @@
 (ns app.core
   (:require [hiccup2.core :as h]
+            [clojure.java.io :as io]
             [app.pages.index :as index-page]
+            [app.pages.404 :as not-found-page]
             [app.pages.art :as art-page]))
+
+(defn generate-404-page
+  [dest-path]
+  (spit
+    (str dest-path "/404.html")
+    (str (h/html (not-found-page/-main-page)))))
 
 (defn generate-pages
   [dest-path routes]
   (let [dest-path (str "./" dest-path)]
-    (.mkdir (java.io.File. dest-path))
+    (.mkdir (io/file dest-path))
+    (generate-404-page dest-path)
     (doseq [route routes]
       (let [page-path (str dest-path (:path route))]
-        (.mkdir (java.io.File. page-path))
+        (.mkdir (io/file page-path))
         (spit
           (str page-path "/index.html")
           (str (h/html ((:page-component route)))))))))
