ef2464f09ea3f3b4f8559567e1ebeb86ac613f39
youkwhd <lolywk@tutanota.com>
2023-09-25 23:13:49 +0700

    chore: add logging

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 
 set -e
 
+echo "[INFO ::] Generating web pages..."
 clj -M -m app.core
+
+echo "[INFO ::] Moving out resources..."
 cp -r resources/* dist/
 cp src/styles/font/* dist/
+
+echo "[INFO ::] Generating css..."
 stylus src/styles/core/wrapper.styl --out dist/global.css
