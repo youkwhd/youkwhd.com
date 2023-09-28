414a92719aece13b9d5384f9ae447320ce6aa2bd
youkwhd <lolywk@tutanota.com>
2023-09-29 00:21:21 +0700

    docs: add todos

--- src/app/layouts/main.clj
+++ src/app/layouts/main.clj
@@ -1,12 +1,12 @@
 (ns app.layouts.main
   (:require [app.components.head :as head]
             [app.components.header :as header]
-            [app.components.footer :as footer]
-            [hiccup2.core :as h]))
+            [app.components.footer :as footer]))
 
 (defn -main-layout
   ;; TODO: description from layout
   [components]
+  ;; TODO: add !DOCTYPE html
   [:html
    (head/-main-component "youkwhd" "youkwhd's page")
    [:body
