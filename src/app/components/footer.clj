dee27a8a2d001e835b0ee4e3787e5acccd776195
youkwhd <lolywk@tutanota.com>
2024-01-11 20:32:32 +0700

    chore: add more links onto footer

--- src/app/components/footer.clj
+++ src/app/components/footer.clj
@@ -13,5 +13,26 @@
        "https://raw.githubusercontent.com/youkwhd/youkwhd.com/master/COPYING",
        :target "_blank", 
        :rel "noopener noreferrer"} 
-      "AGPL v3"] 
-     "."]]])
+      "AGPL v3"]]
+    [:small " | "]
+    [:small "See "
+     [:a
+      {:href
+       "https://github.com/youkwhd/youkwhd.com",
+       :target "_blank", 
+       :rel "noopener noreferrer"} 
+      "source code"]
+     " of this site"]
+    [:small " | "]
+    [:small "Sign "
+     [:a
+      {:href
+       "#",
+       ; :target "_blank", 
+       ; :rel "noopener noreferrer"
+       } 
+      "Guestbook"]
+     " ❗"]
+    [:small " | "]
+    [:small "팬케이크 YWK"]
+    ]])
